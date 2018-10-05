a1ac72cad2fd263cc58c23f62b7f75d6493e819f
Pierre <pier.engelstein@outlook.com>
2018-10-05 11:27:11 +0200

    First Commit

--- main.py
+++
@@ -1,9 +0,0 @@
-from Tkinter import *
-from Display import Display
-
-root = Tk()
-root.title("string")
-
-c = Canvas(root, width = 1200, height = 600, bg='#846298')
-
-dis = Display(root=root, canvas=c, height = 600)
